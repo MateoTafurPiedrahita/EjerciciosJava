91b2ef40098a88a2fdfa7951c77b1a2da19d144f
MateoTafurPiedrahita <u20242226684@usco.edu.co>
2025-03-04 19:43:35 -0500

    Update Sw1.java

--- src/Sw1.java
+++ src/Sw1.java
@@ -6,37 +6,58 @@ public class Sw1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcion;
-        System.out.println(" Menú areas");
-        System.out.println("1 - Circulo ");
-        System.out.println("2 - triangulo");
-        System.out.println("3 - rectangulo ");
-        System.out.println("4 - cuadrado ");
-        System.out.println("9 - salir");
-        System.out.println(" Ingrese la opcion ");
-        opcion=sc.nextInt();
+
+        // Mostrar el menú de opciones
+        System.out.println(" Menú de Áreas");
+        System.out.println("1 - Círculo ");
+        System.out.println("2 - Triángulo");
+        System.out.println("3 - Rectángulo ");
+        System.out.println("4 - Cuadrado ");
+        System.out.println("9 - Salir");
+        System.out.println("Ingrese la opción: ");
+        opcion = sc.nextInt();
+
         switch (opcion) {
             case 1: 
-            System.out.println("valor del radio: ");
-            int radio=sc.nextInt();
-            double area=Math.PI*Math.pow(radio, 2);
-            System.out.println("Area= "+area );
-            break;
-            case 2:
-            System.out.println("Area del triangulo");
-            break;
-            case 3:
-            System.out.println("Area del rectangulo");
-            break;
-            case 4:
-            System.out.println("Area del cuadrado");
-            break;
-            case 9: 
-            System.out.println("Gracias por usar este programa");
-            break;
-            default:
-            System.out.println(" Opcion equivocada!");
-        
-        }
-    }
+                System.out.println("Ingrese el valor del radio: ");
+                double radio = sc.nextDouble();
+                double areaCirculo = Math.PI * Math.pow(radio, 2);
+                System.out.println("Área del círculo = " + areaCirculo);
+                break;
 
+            case 2:
+                System.out.println("Ingrese la base del triángulo: ");
+                double baseTriangulo = sc.nextDouble();
+                System.out.println("Ingrese la altura del triángulo: ");
+                double alturaTriangulo = sc.nextDouble();
+                double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+                System.out.println("Área del triángulo = " + areaTriangulo);
+                break;
+
+            case 3:
+                System.out.println("Ingrese la base del rectángulo: ");
+                double baseRectangulo = sc.nextDouble();
+                System.out.println("Ingrese la altura del rectángulo: ");
+                double alturaRectangulo = sc.nextDouble();
+                double areaRectangulo = baseRectangulo * alturaRectangulo;
+                System.out.println("Área del rectángulo = " + areaRectangulo);
+                break;
+
+            case 4:
+                System.out.println("Ingrese el lado del cuadrado: ");
+                double lado = sc.nextDouble();
+                double areaCuadrado = lado * lado;
+                System.out.println("Área del cuadrado = " + areaCuadrado);
+                break;
+
+            case 9: 
+                System.out.println("Gracias por usar este programa");
+                break;
+
+            default:
+                System.out.println("¡Opción equivocada!");
+        }
+
+        sc.close(); // Cerrar Scanner
+    }
 }
