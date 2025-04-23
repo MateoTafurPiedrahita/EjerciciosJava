1ef2e7533fc996870b76f02e964bfab85a36443e
MateoTafurPiedrahita <u20242226684@usco.edu.co>
2025-04-22 19:08:47 -0500

    Add files via upload

---
+++ POO/Ejercicio8b2/SinMatricula.java
@@ -0,0 +1,7 @@
+package Ejercicio8b2;
+
+public class SinMatricula extends RuntimeException {
+    public SinMatricula(String message) {
+        super(message);
+    }
+}
---
+++ POO/Ejercicio8b2/VamosAlaQuiebra.java
@@ -0,0 +1,7 @@
+package Ejercicio8b2;
+
+public class VamosAlaQuiebra extends RuntimeException {
+    public VamosAlaQuiebra(String message) {
+        super(message);
+    }
+}
